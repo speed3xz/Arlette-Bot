8201161da60f0efd4e0b5973c1b7aacd93c4f8ad
Speed3xz 🎀 <211885527+speed3xz@users.noreply.github.com>
2025-10-09 20:04:39 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.dev = "© powered by speed3xz"
 global.author = "© made with by speed3xz"
 global.etiqueta = "speed3xz"
 global.currency = "Arlette-Coins 🎀"
-global.banner = "https://raw.githubusercontent.com/speed3xz/Storage/main/Arlette-Bot/3e1f8fd2d7e856012e2e5f0c70abfb91.jpg"
+global.banner = "https://raw.githubusercontent.com/speed3xz/Storage/main/Arlette-Bot/b859e5b0780d3eb3f3349f69ab524bcc.jpg"
 global.icono = "https://raw.githubusercontent.com/speed3xz/Storage/main/Arlette-Bot/b859e5b0780d3eb3f3349f69ab524bcc.jpg"
 global.catalogo = fs.readFileSync('./menu/banner.jpg')
 

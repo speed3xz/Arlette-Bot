ec37b0675ec992f7b686fbc17d8030e507203aa6
Speed3xz 🎀 <211885527+speed3xz@users.noreply.github.com>
2025-10-10 12:52:32 -0600

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,32 +1,45 @@
-import fetch from 'node-fetch';
+import fs from 'fs'
+import { WAMessageStubType } from '@whiskeysockets/baileys'
 
-export async function before(m, { conn, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return true;
-
-  try {
-    const chat = global.db.data.chats[m.chat];
-    const defaultImageUrl = 'https://files.catbox.moe/tj7jjy.png';
-    let img;
-
-    // Si existe una imagen personalizada en base64, úsala. Si no, usa la por defecto desde URL
-    if (chat.customWelcomeImg) {
-      img = Buffer.from(chat.customWelcomeImg, 'base64');
-    } else {
-      img = await (await fetch(defaultImageUrl)).buffer();
-    }
-
-    const welcomeTextContent = chat.welcomeMessage || global.welcom1;
-    const newUser = m.messageStubParameters[0];
-    const welcomeText = `✿ *Bienvenido* a ${groupMetadata.subject}\n✰ @${newUser.split('@')[0]}\n\n${welcomeTextContent}\n\n> ✐ Puedes usar */help* para ver la lista de comandos.`;
-
-    if (chat.welcome && m.messageStubType === 27) {
-      await conn.sendMessage(m.chat, {
-        image: img,
-        caption: welcomeText,
-        mentions: [newUser]
-      }, { quoted: m });
-    }
-  } catch (e) {
-    console.error('Error en la bienvenida:', e);
-  }
+async function generarBienvenida({ conn, userId, groupMetadata, chat }) {
+const username = `@${userId.split('@')[0]}`
+const pp = await conn.profilePictureUrl(userId, 'image').catch(() => 'https://raw.githubusercontent.com/speed3xz/Storage/refs/heads/main/Arlette-Bot/b75b29441bbd967deda4365441497221.jpg')
+const fecha = new Date().toLocaleDateString("es-ES", { timeZone: "America/Mexico_City", day: 'numeric', month: 'long', year: 'numeric' })
+const groupSize = groupMetadata.participants.length + 1
+const desc = groupMetadata.desc?.toString() || 'Sin descripción'
+const mensaje = (chat.sWelcome || '૮꒰ ˶• ᴗ •˶꒱ა Disfruta tu estadía en el grupo!').replace(/{usuario}/g, `${username}`).replace(/{grupo}/g, `*${groupMetadata.subject}*`).replace(/{desc}/g, `${desc}`)
+const caption = `❀ Bienvenido a *"_${groupMetadata.subject}_"*\n✰ ${username}\n\n${mensaje}\n\n\n> *➮ Puedes usar _/help_ para ver la lista de comandos.*`
+return { pp, caption, mentions: [userId] }
 }
+async function generarDespedida({ conn, userId, groupMetadata, chat }) {
+const username = `@${userId.split('@')[0]}`
+const pp = await conn.profilePictureUrl(userId, 'image').catch(() => 'https://raw.githubusercontent.com/speed3xz/Storage/refs/heads/main/Arlette-Bot/b75b29441bbd967deda4365441497221.jpg')
+const fecha = new Date().toLocaleDateString("es-ES", { timeZone: "America/Mexico_City", day: 'numeric', month: 'long', year: 'numeric' })
+const groupSize = groupMetadata.participants.length - 1
+const desc = groupMetadata.desc?.toString() || 'Sin descripción'
+const mensaje = (chat.sBye || '-1 homosexual 🥺').replace(/{usuario}/g, `${username}`).replace(/{grupo}/g, `${groupMetadata.subject}`).replace(/{desc}/g, `*${desc}*`)
+const caption = `❀ Adiós de *"_${groupMetadata.subject}_"*\n✰ ${username}\n\n${mensaje}\n\n> *➮ Puedes usar _/help_ para ver la lista de comandos.*`
+return { pp, caption, mentions: [userId] }
+}
+let handler = m => m
+handler.before = async function (m, { conn, participants, groupMetadata }) {
+if (!m.messageStubType || !m.isGroup) return !0
+const primaryBot = global.db.data.chats[m.chat].primaryBot
+if (primaryBot && conn.user.jid !== primaryBot) throw !1
+const chat = global.db.data.chats[m.chat]
+const userId = m.messageStubParameters[0]
+if (chat.welcome && m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+const { pp, caption, mentions } = await generarBienvenida({ conn, userId, groupMetadata, chat })
+rcanal.contextInfo.mentionedJid = mentions
+await conn.sendMessage(m.chat, { image: { url: pp }, caption, ...rcanal }, { quoted: null })
+try { fs.unlinkSync(img) } catch {}
+}
+if (chat.welcome && (m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_LEAVE)) {
+const { pp, caption, mentions } = await generarDespedida({ conn, userId, groupMetadata, chat })
+rcanal.contextInfo.mentionedJid = mentions
+await conn.sendMessage(m.chat, { image: { url: pp }, caption, ...rcanal }, { quoted: null })
+try { fs.unlinkSync(img) } catch {}
+}}
+
+export { generarBienvenida, generarDespedida }
+export default handler

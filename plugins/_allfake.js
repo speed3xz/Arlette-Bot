fa0341e6f2b4590d019d7d6a2f4f884580333e43
Arlette Xz 🦇 <angelcortes7r@gmail.com>
2025-11-06 16:03:44 -0600

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -31,178 +31,177 @@ global.packsticker = `┊ Speed3xz Team\n⤷ https://github.com/speed3xz\n\n┊I
 global.packsticker2 = `┊Bot 🎀\n┊⤷${botname} \n\n┊Usuario:\n┊⤷${nombre}`
 
 // Variables rcanal del primer código
-global.idchannel = '120363402095978084@newsletter'
-global.namechannel = '【 ✰ 】𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 - 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗖𝗵𝗮𝗻𝗻𝗲𝗹'
-global.iconorcanal = 'https://speed3xz.bot.nu/img/IMG_4580.jpeg'
-let icono = 'https://speed3xz.bot.nu/storage/img/IMG_5010.jpeg'
-let iconoden = 'https://speed3xz.bot.nu/img/IMG_4826.jpeg'
-let iconodev = 'https://speed3xz.bot.nu/img/IMG_4580.jpeg'
+   global.idchannel = '120363402095978084@newsletter'
+    global.namechannel = '【 ✰ 】𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 - 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗖𝗵𝗮𝗻𝗲𝗹'
+    global.iconorcanal = 'https://speed3xz.bot.nu/img/IMG_4580.jpeg'
+    let icono = 'https://speed3xz.bot.nu/img/IMG_4580.jpeg'
+    let iconoden = 'https://speed3xz.bot.nu/img/IMG_4580.jpeg'
+    let iconodev = 'https://speed3xz.bot.nu/img/IMG_4580.jpeg'
 
-// Definir las variables globales rcanal del primer código
-global.rcanalw = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: global.idchannel,
-      serverMessageId: 100,
-      newsletterName: global.namechannel,
-    },
-    externalAdReply: {
-      title: '𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 | 𝗣𝗿𝗲𝗺𝗶𝘂𝗺⏤͟͟͞͞🎀',
-      body: '',
-      mediaUrl: null,
-      description: null,
-      previewType: "PHOTO",
-      thumbnailUrl: icono,
-      mediaType: 1,
-      renderLargerThumbnail: false,
-    },
-  },
-}
-
-global.rcanalden2 = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: global.idchannel,
-      serverMessageId: 100,
-      newsletterName: global.namechannel,
-    },
-  },
-}
-
-global.rcanalx = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: global.idchannel,
-      serverMessageId: 100,
-      newsletterName: global.namechannel,
-    },
-    externalAdReply: {
-      title: '【 ✰ 】𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 - 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗖𝗵𝗮𝗻𝗻𝗲𝗹',
-      body: '',
-      mediaUrl: null,
-      description: null,
-      previewType: "PHOTO",
-      thumbnailUrl: icono,
-      mediaType: 1,
-      renderLargerThumbnail: false,
-    },
-  },
-}
-
-global.rcanalr = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: global.idchannel,
-      serverMessageId: 100,
-      newsletterName: global.namechannel,
-    },
-    externalAdReply: {
-      title: '【 ✰ 】𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 - 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗖𝗵𝗮𝗻𝗻𝗲𝗹',
-      body: '',
-      mediaUrl: null,
-      description: null,
-      previewType: "PHOTO",
-      thumbnailUrl: icono,
-      mediaType: 1,
-      renderLargerThumbnail: false,
-    },
-  },
-}
-
-global.rcanalden = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: global.idchannel,
-      serverMessageId: 100,
-      newsletterName: global.namechannel,
-    },
-    externalAdReply: {
-      title: '🔓 𝗔𝗰𝗰𝘀𝗲𝘀𝗼 𝗡𝗼 𝗣𝗲𝗿𝗺𝗶𝘁𝗶𝗱𝗼',
-      body: '',
-      mediaUrl: null,
-      description: null,
-      previewType: "PHOTO",
-      thumbnailUrl: iconoden,
-      mediaType: 1,
-      renderLargerThumbnail: false,
-    },
-  },
-}
-
-global.rcanaldev = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: global.idchannel,
-      serverMessageId: 100,
-      newsletterName: global.namechannel,
-    },
-    externalAdReply: {
-      title: '𝗔𝗿𝗹𝗲𝘁𝘁𝗲 𝗫𝘇',
-      body: '',
-      mediaUrl: null,
-      description: null,
-      previewType: 'PHOTO',
-      thumbnailUrl: iconodev,
-      mediaType: 1,
-      renderLargerThumbnail: false,
-    },
-  },
-}
-  
-global.fkontak = { 
-  key: { 
-    participants: "0@s.whatsapp.net", 
-    "remoteJid": "status@broadcast", 
-    "fromMe": false, 
-    "id": "Halo" 
-  }, 
-  "message": { 
-    "contactMessage": { 
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+    global.rcanalw = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: global.idchannel,
+                serverMessageId: 100,
+                newsletterName: global.namechannel,
+            },
+            externalAdReply: {
+                title: '【 ✰ 】𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 - 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗖𝗵𝗮𝗻𝗲𝗹',
+                body: '',
+                mediaUrl: null,
+                description: null,
+                previewType: "PHOTO",
+                thumbnailUrl: icono,
+                mediaType: 1,
+                renderLargerThumbnail: false,
+            },
+        },
     }
-  }, 
-  "participant": "0@s.whatsapp.net" 
-}
 
-global.rcanal = { 
-  contextInfo: { 
-    isForwarded: true, 
-    forwardedNewsletterMessageInfo: { 
-      newsletterJid: channelRD.id, 
-      serverMessageId: '', 
-      newsletterName: channelRD.name 
-    }, 
-    externalAdReply: { 
-      title: botname, 
-      body: dev, 
-      mediaUrl: null, 
-      description: null, 
-      previewType: "PHOTO", 
-      thumbnail: await (await fetch(icono)).buffer(), 
-      sourceUrl: redes, 
-      mediaType: 1, 
-      renderLargerThumbnail: false 
-    }, 
-    mentionedJid: null 
-  }
-}
+    global.rcanalden2 = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: global.idchannel,
+                serverMessageId: 100,
+                newsletterName: global.namechannel,
+            },
+        },
+    }
+
+    global.rcanalx = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: global.idchannel,
+                serverMessageId: 100,
+                newsletterName: global.namechannel,
+            },
+            externalAdReply: {
+                title: '【 ✰ 】𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 - 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗖𝗵𝗮𝗻𝗲𝗹',
+                body: '',
+                mediaUrl: null,
+                description: null,
+                previewType: "PHOTO",
+                thumbnailUrl: icono,
+                mediaType: 1,
+                renderLargerThumbnail: false,
+            },
+        },
+    }
+
+    global.rcanalr = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: global.idchannel,
+                serverMessageId: 100,
+                newsletterName: global.namechannel,
+            },
+            externalAdReply: {
+                title: '【 ✰ 】𝗦𝗽𝗲𝗲𝗱𝟯𝘅𝘇 𝗧𝗲𝗮𝗺 - 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗖𝗵𝗮𝗻𝗲𝗹',
+                body: '',
+                mediaUrl: null,
+                description: null,
+                previewType: "PHOTO",
+                thumbnailUrl: icono,
+                mediaType: 1,
+                renderLargerThumbnail: false,
+            },
+        },
+    }
+
+    global.rcanalden = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: global.idchannel,
+                serverMessageId: 100,
+                newsletterName: global.namechannel,
+            },
+            externalAdReply: {
+                title: '🔓 𝗔𝗰𝗰𝘀𝗲𝘀𝗼 𝗡𝗼 𝗣𝗲𝗿𝗺𝗶𝘁𝗶𝗱𝗼',
+                body: '',
+                mediaUrl: null,
+                description: null,
+                previewType: "PHOTO",
+                thumbnailUrl: iconoden,
+                mediaType: 1,
+                renderLargerThumbnail: false,
+            },
+        },
+    }
+
+    global.rcanaldev = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: global.idchannel,
+                serverMessageId: 100,
+                newsletterName: global.namechannel,
+            },
+            externalAdReply: {
+                title: '🛠️ 𝗗𝗲𝘃',
+                body: '',
+                mediaUrl: null,
+                description: null,
+                previewType: 'PHOTO',
+                thumbnailUrl: iconodev,
+                mediaType: 1,
+                renderLargerThumbnail: false,
+            },
+        },
+    }
+  
+    global.fkontak = { 
+        key: { 
+            participants: "0@s.whatsapp.net", 
+            remoteJid: "status@broadcast", 
+            fromMe: false, 
+            id: "Halo" 
+        }, 
+        message: { 
+            contactMessage: { 
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+            }
+        }, 
+        participant: "0@s.whatsapp.net" 
+    }
+
+    global.rcanal = { 
+        contextInfo: { 
+            isForwarded: true, 
+            forwardedNewsletterMessageInfo: { 
+                newsletterJid: channelRD.id, 
+                serverMessageId: '', 
+                newsletterName: channelRD.name 
+            }, 
+            externalAdReply: { 
+                title: nombreBot, 
+                body: dev, 
+                mediaUrl: null, 
+                description: null, 
+                previewType: "PHOTO", 
+                thumbnail: await (await fetch(icono)).buffer(), 
+                sourceUrl: redes, 
+                mediaType: 1, 
+                renderLargerThumbnail: false 
+            }, 
+            mentionedJid: null 
+        }
+    }
 }
 
 export default handler
 
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
+    return list[Math.floor(Math.random() * list.length)]
 }
 
 async function getRandomChannel() {
-  let randomIndex = Math.floor(Math.random() * global.canalIdM.length)
-  let id = global.canalIdM[randomIndex]
-  let name = global.canalNombreM[randomIndex]
-  return { id, name }
-  }
+    let randomIndex = Math.floor(Math.random() * global.canalIdM.length)
+    let id = global.canalIdM[randomIndex]
+    let name = global.canalNombreM[randomIndex]
+    return { id, name }
+}

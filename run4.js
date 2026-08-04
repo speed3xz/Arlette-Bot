af39dffc156ff3a8b6c512d4d4857a668c0a232b
猫   ۪   ੭୧  ˖   Arlette Xz <angelcortes7r@gmail.com>
2026-08-04 13:25:19 -0600

    Delete run4.js

--- run4.js
+++
@@ -1,5 +0,0 @@
-import { spawn } from 'child_process';
-console.log('Welcome to your server!\nType your command and send it to your server on "type a command..."')
-spawn('bash', [], {
-  stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-});

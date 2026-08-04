ff456c7c80c04a00da57ed4d5217ffca9001e771
猫   ۪   ੭୧  ˖   Arlette Xz <angelcortes7r@gmail.com>
2026-08-04 13:47:19 -0600

    Refactor settings.js and update global variables

--- settings.js
+++ settings.js
@@ -1,76 +1,35 @@
 import { watchFile, unwatchFile } from "fs"
-import chalk from "chalk"
 import { fileURLToPath } from "url"
 import fs from "fs"
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumber = "" //Ejemplo: 573218138672
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.botNumber = ""
 
 global.owner = [
-"573114910796",
-"556796601394",
-"5215665619261",
-"19106335998",
-"819095203873",
-"16503058299",
-"5216671548329",
+  "573114910796",
+  "819095203873",
+  "5216671548329"
 ]
 
 global.suittag = ["573114910796"] 
 global.prems = []
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.libreria = "Baileys Multi Device"
-global.vs = "1.9.3 VIP"
+global.vs = "V2"
 global.nameqr = "Arlette-Bot"
 global.sessions = "Sessions/Principal"
-global.jadi = "Sessions/SubBot"
-global.yukiJadibts = true
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.botname = "Arlette-Bot"
-global.textbot = "Arlette-Bot, made with by speed3xz"
-global.dev = "© powered by speed3xz"
-global.author = "© made with by speed3xz"
-global.etiqueta = "speed3xz"
-global.currency = "Arlette-Coins 🪙"
-global.banner = "https://speed3xz.bot.nu/storage/img/IMG_5018.jpeg"
-global.icono = "https://speed3xz.bot.nu/img/IMG_4580.jpeg"
-global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.group = "https://chat.whatsapp.com/CN8JtNy0BTCHb2v5009AL5"
-global.channel = "https://whatsapp.com/channel/0029VbAmwbQBqbr587Zkni1a"
-global.github = "https://github.com/speed3xz/Arlette-Bot"
-global.gmail = "211885527+speed3xz@users.noreply.github.com"
-global.ch = {
-ch1: "120363402095978084@newsletter"
-}
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.APIs = {
-xyro: { url: "https://xyro.site", key: null },
-yupra: { url: "https://api.yupra.my.id", key: null },
-vreden: { url: "https://api.vreden.web.id", key: null },
-delirius: { url: "https://api.delirius.store", key: null },
-zenzxz: { url: "https://api.zenzxz.my.id", key: null },
-siputzx: { url: "https://api.siputzx.my.id", key: null }
-}
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.textbot = "Arlette-Bot, made with love by Arlette Xz"
+global.dev = "© powered by Arlette Xz"
+global.author = "© made with love by Arlette Xz"
+global.etiqueta = "Arlette Xz"
+global.currency = "Coins 🍒"
+global.banner = "https://raw.githubusercontent.com/speed3xz/Storage/main/Arlette-Bot/b859e5b0780d3eb3f3349f69ab524bcc.jpg"
+global.icono = "https://files.evogb.win/XD8JxJ.jpg"
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
-unwatchFile(file)
-console.log(chalk.redBright("Update 'settings.js'"))
-import(`${file}?update=${Date.now()}`)
+  unwatchFile(file)
+  console.log("Update 'settings.js'")
+  import(`${file}?update=${Date.now()}`)
 })

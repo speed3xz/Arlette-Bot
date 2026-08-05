bd29ba5d782d5ed647a033432f5933d1ba1391f2
猫   ۪   ੭୧  ˖   Arlette Xz <angelcortes7r@gmail.com>
2026-08-05 03:57:46 -0600

    Refactor premium user notification logic

--- plugins/_userPrem.js
+++ plugins/_userPrem.js
@@ -1,12 +1,20 @@
 const handler = (m) => m
+
 export async function all(m) {
-for (const user of Object.values(global.db.data.users)) {
-if (user.premiumTime != 0 && user.premium) {
-if (new Date() * 1 >= user.premiumTime) {
-user.premiumTime = 0
-user.premium = false
-const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user)
-const usuarioJid = JID.split`@`[0]
-const textoo = `「✐」@${usuarioJid} Se agotó tu tiempo como usuario premium`
-await this.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: m })
-}}}}
+    for (const user of Object.values(global.db.data.users)) {
+        if (user.premiumTime != 0 && user.premium) {
+            if (new Date() * 1 >= user.premiumTime) {
+                user.premiumTime = 0
+                user.premium = false
+                
+                const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user)
+                if (!JID) continue
+
+                const usuarioJid = JID.split('@')[0]
+                const texto = `⭐ *Suscripción Premium*\n@${usuarioJid} tu tiempo como usuario premium ha finalizado.`
+
+                await this.sendMessage(JID, { text: texto, mentions: [JID] }, { quoted: m })
+            }
+        }
+    }
+}

b5433fbd56e02e6d1cca9061f1e1280dfd25138a
猫   ۪   ੭୧  ˖   Arlette Xz <angelcortes7r@gmail.com>
2026-08-05 05:04:29 -0600

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,31 +1,27 @@
 import PhoneNumber from 'awesome-phonenumber'
 
 async function handler(m, { conn }) {
-  m.react('👑')
+    let ownerNumber = global.owner?.[1] || '819095203873'
+    if (Array.isArray(ownerNumber)) ownerNumber = ownerNumber[0]
 
-  const contactos = [
-    {
-      numero: '573114910796',
-      nombre: 'Arlette Xzy ✨️',
-      cargo: 'Dueña Principal',
-      nota: 'Creadora de la Bot',
-      region: '🇨🇴 Colombia',
-      web: 'https://github.com/speed3xz',
-      biografia: await conn.fetchStatus('573114910796@s.whatsapp.net').then(res => res.status).catch(_ => 'Sin biografía')
-    }
-  ]
+    let number = ownerNumber.replace(/[^0-9]/g, '')
+    let name = global.etiqueta || 'Arlette Xz'
+    let formattedNumber = PhoneNumber('+' + number).getNumber('international') || '+' + number
 
-  const contactArray = contactos.map(c => [
-    c.numero,
-    c.nombre,
-    c.cargo,
-    c.nota,
-    c.region,
-    c.web,
-    c.biografia
-  ])
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name.replace(/\n/g, '\\n')};;;
+FN:${name.replace(/\n/g, '\\n')}
+item1.TEL;waid=${number}:${formattedNumber}
+END:VCARD`.trim()
 
-  await sendContactArray(conn, m.chat, contactArray, m)
+    await conn.sendMessage(m.chat, {
+        contacts: {
+            displayName: name,
+            contacts: [{ vcard, displayName: name }]
+        }
+    }, { quoted: m })
 }
 
 handler.help = ['owner', 'creador', 'creator']
@@ -33,39 +29,3 @@ handler.tags = ['info']
 handler.command = ['owner', 'creator', 'creador', 'dueño', 'creadora', 'dueña']
 
 export default handler
-
-async function sendContactArray(conn, jid, data, quoted, options) {
-  if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
-  let contacts = []
-  for (let [number, name, title, note, email, region, url, bio] of data) {
-    number = number.replace(/[^0-9]/g, '')
-    let vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-item.ORG:${title}
-item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
-item1.X-ABLabel:${note}
-item2.EMAIL;type=INTERNET:${email}
-item2.X-ABLabel:Correo
-item3.ADR:;;${region};;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:Región
-item4.URL:${url}
-item4.X-ABLabel:Sitio Web
-item5.X-ABLabel:${bio}
-END:VCARD`.trim()
-    contacts.push({ vcard, displayName: name })
-  }
-
-  return await conn.sendMessage(jid, {
-    contacts: {
-      displayName: 'Propietarios del Bot',
-      contacts,
-    }
-  }, {
-    quoted,
-    ...options
-  })
-}
